231d413e27deaa3854a07c7736f07fbfef76cf5b
gowtham-bonu-simform <gowtham@simformsolutions.com>
2023-04-06 15:58:51 +0530

    TE7-T124:RailsRouting:Ref:Refactor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,11 @@
 class OrdersController < ApplicationController
   before_action :get_order, only: [:edit, :update, :destroy]
 
-  def get_order
-    @order = Order.find(params[:id])
+  def index
+    @products = Product.all
   end
+
+  def show; end
 
   def new
     @order = Order.new
@@ -12,38 +14,37 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.save
-      redirect_to root_path
+      redirect_to root_path, notice: "you have successfully created an order"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @order.update(order_params)
-      @product = Product.find(params[:product_id])
-      redirect_to product_path(@product)
+      product = Product.find(params[:product_id])
+      redirect_to product_path(product), notice: "you have successfully updated the order"
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  def index
-    @products = Product.all
-  end
-
-  def show
-  end
-
   def destroy
-    @order.destroy
-    @product = Product.find(params[:product_id])
-    redirect_to product_path(@product), status: :see_other
+    product = Product.find(params[:product_id])
+    if @order.destroy
+      redirect_to product_path(product), status: :see_other, notice: "you have successfully deleted the order"
+    else
+      redirect_to product_path(product), status: :unprocessable_entity, alert: "The delete action didn't work.."
+    end
   end
 
   private
+
+  def get_order
+    @order = Order.find(params[:id])
+  end
 
   def order_params
     params.require(:order).permit(:name, :status, :quantity, :product_id)
